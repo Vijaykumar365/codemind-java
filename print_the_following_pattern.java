6a7e53af0fc4932af98c4db5d3e70ee2f2f9b178
Vijaykumar <112695699+Vijaykumar365@users.noreply.github.com>
2022-09-13 13:02:31 +0530

    import java.util.*; class pattern {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a;         a=sc.nextInt();         for(int i=a;i>=1;i--)         {             for(int j=1;j<=i;j++)         {
            System.out.print(j);
                
            }
            System.out.println();
            }
        }
    }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class pattern
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        for(int i=a;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+        {
+        
+        System.out.print(j);
+            
+        }
+        System.out.println();
+        }
+    }
+}
