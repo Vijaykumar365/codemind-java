03c21331d23758cf6c815e2178830ec116f648da
Vijaykumar <112695699+Vijaykumar365@users.noreply.github.com>
2022-09-17 15:54:57 +0530

    import java.util.*; class array {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,x[],i,count=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)          x[i]=sc.nextInt();
            for(i=0;i<n-1;i++)
            {
                if(x[i+1]<=x[i])
                {
                    System.out.println("no");
                    System.exit(0);
                }}
                 System.out.println("yes");
        
            
            
        
        
        }
    }

---
+++ Ascending_array.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class array
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        
+        for(i=0;i<n-1;i++)
+        {
+            if(x[i+1]<=x[i])
+            {
+                System.out.println("no");
+                System.exit(0);
+            }}
+             System.out.println("yes");
+    
+        
+        
+    
+    
+    }
+}
